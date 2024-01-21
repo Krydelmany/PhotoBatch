f04770c3c3f7d70baef37f1396037fd5249d11bd
--global <giclamoraes@gmail.com>
2024-01-20 22:05:57 -0300

    Teste gitç

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -4,4 +4,14 @@ using namespace std;
 
 main(){
     cout << "Hello World!";
+    int you, okay;
+
+    cin>>you;
+    cin>>okay;
+    
+    if (you == okay){
+        cout << "Git test";
+    }
+
+    cout<<you<<okay;
 }
