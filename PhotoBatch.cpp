cc3957f2cabff8e257a55b363bfce2817b9bd733
--global <giclamoraes@gmail.com>
2024-01-21 01:36:57 -0300

    Primeiros arquivos do Photobatch

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 
-using namespace std;
-
-main(){
-    cout << "Hello World!";
+int main()
+{
+    return 0;
 }
---
+++ build/CMakeFiles/PhotoBatch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhotoBatch.dir/PhotoBatch.cpp.obj"
+  "CMakeFiles/PhotoBatch.dir/PhotoBatch.cpp.obj.d"
+  "PhotoBatch.exe"
+  "PhotoBatch.exe.manifest"
+  "PhotoBatch.pdb"
+  "libPhotoBatch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhotoBatch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
